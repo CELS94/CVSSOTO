c6b8d15676d493c11af42ab9ec91667ef8b408bb
USUARIO <USUARIO@DESKTOP-DHFQE8M>
2025-04-03 19:35:23 +0200

    Primera confirmación

---
+++ src/refactorizar/Clase1.java
@@ -0,0 +1,18 @@
+
+package refactorizar;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Celia");
+        System.out.println("Clase 1 ");
+    }
+    
+}
