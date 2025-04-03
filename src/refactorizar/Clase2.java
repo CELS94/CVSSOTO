52df090221e4c1b5eb6ed83121214467796f51ba
USUARIO <USUARIO@DESKTOP-DHFQE8M>
2025-04-03 19:41:19 +0200

    segunda confirmación

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,14 @@
+
+package refactorizar;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Clase2 {
+    public void mensaje(){
+        System.out.println("estas en la rama produccion");
+    
+}
+    
+}
